84b30f5b83c67654e9cfa331487b5ccb6f7703ed
Ben Garrett <code.by.ben@gmail.com>
2023-01-08 11:55:51 +1100

    initial commit

---
+++ keygen.c
@@ -0,0 +1,30 @@
+/* MailBomber Keygen Source Code */
+/* by FaT[BiT] \ TNT!CRACK!TEAM! */
+/* fatzbitz@hotmail.com */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <conio.h>
+
+int main (void)
+{
+   int i;
+   char ch;
+
+   srand(time(NULL));
+   clrscr();
+   printf("Mail Bomber v5.1 - keygen - by FaT[BiT] \\ TNT!\n");
+   printf("----------------------------------------------");
+   do 
+     {
+      printf("\n\nRegistration Code : %d25%d%d2%d5%d0%d1%d%d%d%d\n\n", 0 + rand() % 10, 0 + rand() % 10,
+       0 + rand() % 10, 0 + rand() % 10, 0 + rand() % 10, 0 + rand() % 10, 0 + rand() % 10, 0 + rand() % 10,
+       0 + rand() % 10, 0 + rand() % 10);
+      printf("Generate (Y/N) ?");
+      ch = getche();
+      } while ((ch == 'Y') || (ch == 'y'));
+
+	return 0 ;
+}
+
